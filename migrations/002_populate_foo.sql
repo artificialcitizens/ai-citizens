57b5b3f0016702b2646a4d01a4d31b79dc32909b
Josh Mabry <mabry1985@gmail.com>
2024-08-17 14:50:15 -0700

    add persistent data logic, example migrations

---
+++ migrations/002_populate_foo.sql
@@ -0,0 +1,7 @@
+-- Insert dummy data into the foo table
+INSERT INTO foo (name, value) VALUES
+  ('Alice', 3.14),
+  ('Bob', 2.71),
+  ('Charlie', -1.618),
+  ('David', 1.414),
+  ('Eve', 0);
--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import { useShape } from "@electric-sql/react";
 function App() {
   const { data } = useShape({
-    url: `http://localhost:3000/v1/shape/foo`,
+    url: `http://localhost:3000/v1/shape/checkpoints`,
   });
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <pre>{JSON.stringify(data, null, 4)}</pre>
-        <a href='https://vitejs.dev' target='_blank'>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+      {/* <a href='https://vitejs.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
         <a href='https://react.dev' target='_blank'>
@@ -32,6 +31,7 @@ function App() {
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
+      */}
     </>
   );
 }
