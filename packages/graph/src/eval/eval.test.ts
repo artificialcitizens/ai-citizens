373fa6893659698b5d4176f9db79f73bba81c6cf
Josh Mabry <mabry1985@gmail.com>
2025-02-05 17:45:20 -0800

    add eval graph

--- packages/graph/src/eval/eval.test.ts
+++ packages/graph/src/eval/eval.test.ts
@@ -1,9 +1,22 @@
+import { HumanMessage } from "@langchain/core/messages";
 import { evaluateModels } from "./index.js";
 import test from "ava";
 import "dotenv/config";
 
-test("evaluateModels", async (t) => {
-  const responses = await evaluateModels("What is the capital of the moon?");
+// test("evaluateModels using messages", async (t) => {
+//   const responses = await evaluateModels({
+//     messages: [new HumanMessage("What is the capital of the moon?")],
+//     models: ["gpt-4o-mini", "llama-3.1-8b-instant", "claude-3-opus-20240229"],
+//   });
+//   t.log(responses);
+//   t.pass();
+// });
+
+test("evaluateModels using prompt", async (t) => {
+  const responses = await evaluateModels({
+    prompt: "What is the capital of the moon?",
+    models: ["gpt-3.5-turbo", "gpt-4", "claude-3-opus-20240229", "gemma-7b-it"],
+  });
   t.log(responses);
   t.pass();
 });
--- packages/graph/src/eval/index.ts
+++ packages/graph/src/eval/index.ts
@@ -2,35 +2,44 @@ import { StringOutputParser } from "@langchain/core/output_parsers";
 import { END, START, StateGraph, StateGraphArgs } from "@langchain/langgraph";
 
 import { getModel, Model } from "@ai-citizens/llm";
+import { BaseMessage, HumanMessage } from "@langchain/core/messages";
 
 const parser = new StringOutputParser();
 
-const modelsToEvaluate: Model[] = [
-  "gpt-3.5-turbo",
-  "gpt-4",
-  "claude-3-opus-20240229",
-  "gemma-7b-it",
-];
-
 interface EvalState {
-  prompt: string;
-  responses: { model: string; output: string; score: number }[];
+  messages: BaseMessage[];
+  responses: {
+    model: string;
+    output: string;
+    score?: number;
+    time: number;
+  }[];
 }
 
-const stringReducer = (left = "", right?: string): string => right ?? left;
-
 const responseReducer = (
-  left: { model: string; output: string; score: number }[] = [],
-  right?: { model: string; output: string; score: number }[]
-): { model: string; output: string; score: number }[] => [
-  ...left,
-  ...(right || []),
-];
+  left: {
+    model: string;
+    output: string;
+    score: number;
+    time: number;
+  }[] = [],
+  right?: {
+    model: string;
+    output: string;
+    score: number;
+    time: number;
+  }[]
+): {
+  model: string;
+  output: string;
+  score: number;
+  time: number;
+}[] => [...left, ...(right || [])];
 
 const graphState: StateGraphArgs<EvalState>["channels"] = {
-  prompt: {
-    default: () => "",
-    value: stringReducer,
+  messages: {
+    default: () => [],
+    value: (x: BaseMessage[] = [], y: BaseMessage[] = []) => x.concat(y),
   },
   responses: {
     default: () => [],
@@ -38,38 +47,72 @@ const graphState: StateGraphArgs<EvalState>["channels"] = {
   },
 };
 
-const generateResponse = async (
-  prompt: string,
-  model: Model
-): Promise<string> => {
+const generateResponse = async ({
+  model,
+  messages,
+}: {
+  model: Model;
+  messages?: BaseMessage[];
+}): Promise<string> => {
   const llm = await getModel({ model });
-  const response = await llm.invoke(prompt);
+  const response = await llm.invoke(messages);
   return parser.invoke(response);
 };
 
-const evalBuilder = new StateGraph<EvalState>({ channels: graphState });
+export const createEvalGraph = (modelsToEvaluate: Model[]) => {
+  const evalBuilder = new StateGraph<EvalState>({ channels: graphState });
+  for (const model of modelsToEvaluate) {
+    evalBuilder.addNode(`evaluate-${model}`, async (state) => {
+      const start = performance.now();
+      const response = await generateResponse({
+        model,
+        messages: state.messages,
+      });
+      const end = performance.now();
+      return {
+        responses: [
+          {
+            model,
+            output: response,
+            score: 0,
+            time: end - start,
+          },
+        ],
+      };
+    });
+  }
 
-// Add nodes for each model
-for (const model of modelsToEvaluate) {
-  evalBuilder.addNode(`evaluate-${model}`, async (state) => ({
-    responses: [
-      { model, output: await generateResponse(state.prompt, model), score: 0 },
-    ],
-  }));
-}
+  // Add edges
+  for (const model of modelsToEvaluate) {
+    // @ts-expect-error this is fine
+    evalBuilder.addEdge(START, `evaluate-${model}`);
+    // @ts-expect-error this is fine
+    evalBuilder.addEdge(`evaluate-${model}`, END);
+  }
 
-// Add edges
-for (const model of modelsToEvaluate) {
-  // @ts-expect-error this is fine
-  evalBuilder.addEdge(START, `evaluate-${model}`);
-  // @ts-expect-error this is fine
-  evalBuilder.addEdge(`evaluate-${model}`, END);
-}
+  const graph = evalBuilder.compile();
+  return graph;
+};
 
-const graph = evalBuilder.compile();
-
-export const evaluateModels = async (prompt: string) => {
-  const initialState: Partial<EvalState> = { prompt };
-  const finalState = await graph.invoke(initialState);
+export const evaluateModels = async ({
+  prompt,
+  messages = [],
+  models = [],
+}: {
+  models: Model[];
+  prompt?: string;
+  messages?: BaseMessage[];
+}) => {
+  if (!messages?.length && !prompt) {
+    throw new Error("messages or prompt must be provided");
+  }
+  if (prompt) {
+    messages = [new HumanMessage(prompt)];
+  }
+  const initialState: Partial<EvalState> = {
+    messages,
+  };
+  const graph = createEvalGraph(models);
+  const finalState: EvalState = await graph.invoke(initialState);
   return finalState.responses;
 };
